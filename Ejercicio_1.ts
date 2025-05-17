d62a5d3ab2f94a9257c94ff5897c8759b725836e
Angelo Di Martino <aadimartino.18@est.ucab.edu.ve>
2025-05-16 21:47:57 -0400

    Practica #4 - callback

---
+++ Ejercicio_1.ts
@@ -0,0 +1,46 @@
+type Persona = {
+    nombre: string;
+    fechaNacimiento: Date;
+};
+
+const arregloPersonas: Persona[] = [
+    { nombre: "Juan", fechaNacimiento: new Date("1990-01-01") },
+    { nombre: "Maria", fechaNacimiento: new Date("1985-05-15") },
+    { nombre: "Pedro", fechaNacimiento: new Date("2000-10-20") },
+    { nombre: "Ana", fechaNacimiento: new Date("1995-03-30") },
+    { nombre: "Luis", fechaNacimiento: new Date("1988-07-25") }
+]
+
+function calcularEdad(fecha: Date): number {
+    const fechaActual = new Date();
+    const edad = fechaActual.getFullYear() - fecha.getFullYear();
+    const mes = fechaActual.getMonth() - fecha.getMonth();
+    if (mes < 0 || (mes === 0 && fechaActual.getDate() < fecha.getDate())) {
+        return edad - 1;
+    }
+    return edad;
+}
+
+function mostrarArreglo(texto: string, arreglo: [string, number][]): void {
+    console.log(texto);
+    arreglo.map(persona => {
+        console.log("Nombre: " + persona[0] + ", Edad: " + persona[1]);
+    });
+}
+
+// FUNCION CALLBACK
+function calculoCallback(
+    arreglo: Persona[], 
+    callback: (fecha: Date) => number, 
+    mostrar: (texto: string, arreglo: [string, number][]) => void
+): void{
+    setTimeout(() => {
+        const resultado: [string, number][] = arreglo.map(persona => {
+            const edad = callback(persona.fechaNacimiento);
+            return [persona.nombre, edad];
+        });
+        mostrar("Callback: ", resultado);
+    }, 4000);
+}
+
+calculoCallback(arregloPersonas, calcularEdad, mostrarArreglo);
